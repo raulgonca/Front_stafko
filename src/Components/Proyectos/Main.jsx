e0127eb3c1edb2d11d16deda445f29681113a953
raulgonca <raulgo2003@gmail.com>
2024-04-29 14:11:45 +0200

    29/4 mejoras

--- src/Components/Proyectos/Main.jsx
+++ src/Components/Proyectos/Main.jsx
@@ -3,6 +3,8 @@ import Swal from 'sweetalert2';
 import { FaEdit, FaPlus, FaTimes, FaTrash } from 'react-icons/fa';
 import ProyectoForm from "./ProyectoForm";
 import ProyectoEdit from "./ProyectoEdit";
+//import Sidebar from './Sidebar';
+
 
 const Main = () => {
   const [proyectos, setProyectos] = useState([]);
@@ -92,24 +94,23 @@ const Main = () => {
     setMostrarFormulario(false);
   };
 
+
   const ProyectoCard = ({ proyecto }) => {
     return (
-      <div className="bg-white rounded-lg shadow-lg overflow-hidden transition duration-300 hover:shadow-xl">
-        <div className="p-6 flex flex-col justify-between h-full">
-          <div>
-            <h3 className="text-xl font-semibold text-gray-800 mb-2">{proyecto.nameproject}</h3>
-            <p className="text-gray-600">{proyecto.description}</p>
-          </div>
-          <div className="mt-4 bg-gray-100 py-2 px-4 rounded-lg flex justify-between">
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl">
+        <div className="p-6">
+          <h3 className="text-xl font-semibold text-gray-800 mb-2">{proyecto.nameproject}</h3>
+          <p className="text-gray-600">{proyecto.description}</p>
+          <div className="mt-4 flex justify-between">
             <button
               onClick={() => handleEditarProyecto(proyecto)}
-              className="text-blue-500 hover:text-blue-700 focus:outline-none transition duration-300"
+              className="text-blue-500 hover:text-blue-700 focus:outline-none"
             >
               <FaEdit className="inline-block mr-1" /> Editar
             </button>
             <button
               onClick={() => handleEliminarProyecto(proyecto.id)}
-              className="text-red-500 hover:text-red-700 focus:outline-none transition duration-300"
+              className="text-red-500 hover:text-red-700 focus:outline-none"
             >
               <FaTrash className="inline-block mr-1" /> Eliminar
             </button>
@@ -120,54 +121,52 @@ const Main = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      {mostrarFormulario && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 transition duration-300">
-          <div className="bg-white w-full max-w-lg p-8 rounded-lg shadow-lg relative z-50">
-            <button
-              onClick={handleCloseFormulario}
-              className="absolute top-0 right-0 mt-4 mr-4 text-gray-600 hover:text-gray-800 focus:outline-none"
-            >
-              <FaTimes />
-            </button>
-            <div className="form-container">
-              {proyectoEditar ? (
-                <ProyectoEdit
-                  proyectoInicial={proyectoEditar}
-                  onSubmit={handleCloseFormulario}
-                  onProjectUpdate={handleActualizarProyectos}
-                />
-              ) : (
-                <ProyectoForm
-                  onSubmit={handleCloseFormulario}
-                  onProjectUpdate={handleActualizarProyectos}
-                />
-              )}
+    <div className="flex">
+      {/*<Sidebar />*/ }
+      <div className="flex-1 p-8 ml-64"> {/* Añadimos ml-64 para ajustar el espacio a la izquierda del Sidebar */}
+        <header className="text-center mb-8">
+          <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-2">Gestiona tus Proyectos</h1>
+          <p className="text-lg text-gray-600">Organiza tus proyectos de manera eficiente</p>
+          <button
+            onClick={() => setMostrarFormulario(true)}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full mt-4 focus:outline-none"
+          >
+            <FaPlus className="mr-2" /> Nuevo Proyecto
+          </button>
+        </header>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {proyectos.map((proyecto) => (
+            <ProyectoCard key={proyecto.id} proyecto={proyecto} />
+          ))}
+        </div>
+
+        {mostrarFormulario && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white max-w-lg p-8 rounded-lg shadow-lg">
+              <button
+                onClick={() => setMostrarFormulario(false)}
+                className="absolute top-0 right-0 mt-4 mr-4 text-gray-600 hover:text-gray-800 focus:outline-none"
+              >
+                <FaTimes />
+              </button>
+              <div className="form-container">
+                {proyectoEditar ? (
+                  <ProyectoEdit
+                    proyectoInicial={proyectoEditar}
+                    onSubmit={() => setMostrarFormulario(false)}
+                    onProjectUpdate={getAllProject}
+                  />
+                ) : (
+                  <ProyectoForm
+                    onSubmit={() => setMostrarFormulario(false)}
+                    onProjectUpdate={getAllProject}
+                  />
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      )}
-
-      <header className="text-center mb-8">
-        <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-2">¡Comienza con tus Proyectos!</h1>
-        <p className="text-lg text-gray-600">Gestiona y Crea tus Proyectos con Facilidad</p>
-        <div className="flex justify-center">
-          <button
-            onClick={handleCrearProyecto}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full mt-4 focus:outline-none flex items-center transition duration-300"
-          >
-          <FaPlus className="mr-2" /> Nuevo Proyecto
-          </button>
-        </div>
-      </header>
-
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {proyectos.map((proyecto) => (
-          <ProyectoCard
-            key={proyecto.id}
-            proyecto={proyecto}
-          />
-        ))}
+        )}
       </div>
     </div>
   );
